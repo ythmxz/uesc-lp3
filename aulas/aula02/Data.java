ca0d44eeb6d8318fcf67103b5b4b8d29d72add45
Yuri Thomaz <ytcarvalho14@gmail.com>
2025-04-01 12:03:08 -0300

    Adiciona base do desafio 2

---
+++ aulas/aula02/Data.java
@@ -0,0 +1,25 @@
+package aulas.aula02;
+
+public class Data {
+
+	int dia;
+	int mes;
+	int ano;
+
+	public Data() {} //Data atual
+
+	public Data(int _dia, int _mes, int _ano) {} //Data personalizada
+
+	public Data(String _data) {} //Data em string
+
+	private boolean validarData(Data _data) {
+		return true;
+
+	}
+
+	public static boolean compararDatas(Data _data1, Data _data2) {
+		return true;
+
+	}
+
+}
